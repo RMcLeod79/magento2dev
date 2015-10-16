dd5b27279a680113a54893dc20fbe9eaa83c2058
Richard McLeod <richard@wearejh.com>
2015-10-16 11:38:34 +0100

    Remove htdocs and add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -d "htdocs" ]; then
+    echo "Magento is already installed, run vagrant up"
+    exit 1
+fi
+
+echo "Cloning Magento 2, this can take a while!"
+git clone -b develop git@github.com:magento/magento2.git htdocs
+
+cd htdocs && composer install
+
+vagrant box add magento2 magento2.box
+
+echo "Now run vagrant up and vagrant ssh\n"
+echo "Once logged into the VM run ./install.sh"
