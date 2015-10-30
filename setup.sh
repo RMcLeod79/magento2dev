6c2de31394dea1085ebca786ed5559dcd96c0b0f
Richard McLeod <richard@wearejh.com>
2015-10-30 15:38:14 +0000

    Fix missing semi-colon

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ fi
 echo "Creating PHP7 dev environment"
 mkdir php7
 echo "<?php" > php7/index.php
-echo "phpinfo()" >> php7/index.php
+echo "phpinfo();" >> php7/index.php
 
 echo "Cloning Magento 2, this can take a while!"
 git clone -b develop git@github.com:magento/magento2.git magento2
